5d9da30dbe7053041b287f4f64af16feb1e9783d
Ben Downey <bd@bendowney.net>
2017-03-21 20:48:41 -0700

    white space change in location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,13 +7,13 @@ class Location < ApplicationRecord
   private
 
   def unique_location
-		preexisting_location = self.class.where(
-			address_line_1: self.address_line_1,
-			address_line_2: self.address_line_2,
-			city: self.city,
-			state: self.state,
-			zipcode: self.zipcode,
-		).first
+    preexisting_location = self.class.where(
+      address_line_1: self.address_line_1,
+      address_line_2: self.address_line_2,
+      city: self.city,
+      state: self.state,
+      zipcode: self.zipcode,
+    ).first
 
     errors.add(:location, 'already exists') if preexisting_location
   end
