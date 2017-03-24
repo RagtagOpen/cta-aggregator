b1b94baaf0234939f6dbfb3e665220125a6e3e7e
Ben Downey <bd@bendowney.net>
2017-03-24 05:48:28 -0700

    removes unnecessary filter on location resource

--- app/resources/v1/location_resource.rb
+++ app/resources/v1/location_resource.rb
@@ -2,6 +2,6 @@ module V1
   class LocationResource < JSONAPI::Resource
     attributes :address_line_1, :address_line_2, :city, :state, :zipcode, :notes
 
-    filters :address_line_1, :address_line_2, :city, :state, :zipcode
+    filters :address_line_1, :city, :state, :zipcode
   end
 end
