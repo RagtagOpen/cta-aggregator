d17278444ee06468406f54f0307d0234ee42e16d
Spike Ilacqua <spike@indra.com>
2017-06-24 15:57:29 -0600

    Enable rack-cors outside of production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, :debug => !Rails.env.production?,  :logger => (-> { Rails.logger }) do
   allow do
     origins '*'
     # only allow read actions, at least for now
