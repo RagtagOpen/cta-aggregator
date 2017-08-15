d836b38794e75e67fe6b9f019a6dce7181818466
Ben Downey <BD@BenDowney.net>
2017-08-14 20:01:24 -0700

    Feature/target changes (#57)
    * Add title col to Targets table
    
    * Destroy advocacy_campaign_target when target is destroyed
    
    * Update TargetResource to include title attr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807172203) do
+ActiveRecord::Schema.define(version: 20170813051944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20170807172203) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.uuid     "user_id"
+    t.string   "title"
   end
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
