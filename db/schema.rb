2e6ec0df4635747e18657200c410c40063a4fa77
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-08-21 21:37:21 -0700

    Add admin col to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170813051944) do
+ActiveRecord::Schema.define(version: 20170821030113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,11 +82,12 @@ ActiveRecord::Schema.define(version: 20170813051944) do
   end
 
   create_table "users", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "api_key",         null: false
-    t.string   "password_digest", null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",                           null: false
+    t.string   "api_key",                         null: false
+    t.string   "password_digest",                 null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
     t.index ["api_key"], name: "index_users_on_api_key", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
