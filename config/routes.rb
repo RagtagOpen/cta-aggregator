1aed628a0172f793c894af79460283dba8ce446c
Ben Downey <BD@BenDowney.net>
2017-08-29 20:38:40 -0700

    Rename authentication endpoint (#73)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :v1, defaults: { format: 'json' } do
-    post 'authorize' => 'user_token#create'
+    post 'authentications' => 'user_token#create'
     jsonapi_resources :advocacy_campaigns
     jsonapi_resources :targets
     jsonapi_resources :events
