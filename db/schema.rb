87869d75c3b4469d056d794fb216fc324f22d06d
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-10-17 20:25:41 -0700

    Add share_url attribute to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014202558) do
+ActiveRecord::Schema.define(version: 20171018024140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171014202558) do
     t.datetime "updated_at",         null: false
     t.uuid     "user_id"
     t.text     "identifiers"
+    t.string   "share_url"
   end
 
   create_table "locations", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
