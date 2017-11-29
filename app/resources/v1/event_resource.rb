64dcedeeca92b00634e07d3fc7fb7c956b9ded39
chrisbrickey <cbrickey1@gmail.com>
2017-11-28 16:07:30 -0800

    add filter for origin_system to event resource

--- app/resources/v1/event_resource.rb
+++ app/resources/v1/event_resource.rb
@@ -18,5 +18,8 @@ module V1
       records.unscope(:where).past if value[0] == "true"
     }
 
+    filter :origin_system # This enables: http://example.com/events?filter[origin_system]=5calls
+    # issue: events with and without filters do not appear when hosted locally (no issue for deployed version)
+
   end
 end
