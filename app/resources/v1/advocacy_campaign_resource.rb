d5609e649bb9d9dcbc4531f3d29bb05df352e330
chrisbrickey <cbrickey1@gmail.com>
2017-12-07 18:59:21 -0800

    write custom filter for target_list to filter advocacy campaigns by target id

--- app/resources/v1/advocacy_campaign_resource.rb
+++ app/resources/v1/advocacy_campaign_resource.rb
@@ -17,6 +17,21 @@ module V1
 
     filter :origin_system
 
+    filter :target_list, apply: ->(records, value, _options) {
+      target_campaigns = []
+
+      records.each do |campaign|
+        campaign.target_list.each do |target_object|
+          target_campaigns << campaign if target_object.id == value[0]
+        end
+      end
+
+      target_campaigns.uniq
+    }
+
+    #SQL query for finding targets by id
+    # SELECT  "targets".* FROM "targets" WHERE "targets"."id" = '98531f36-96b3-4083-ae6d-2fc07a399e71'
+
     before_create do
       @model.user_id = context[:current_user].id if @model.new_record?
     end
